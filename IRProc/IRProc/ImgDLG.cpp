06d909eb00a01859e72da7d5bb02f189d6d6ee73
Kefeng Li <seafrog1984@hotmail.com>
2017-06-07 23:16:11 +0800

    v1.3 画矩形

--- IRProc/IRProc/ImgDLG.cpp
+++ IRProc/IRProc/ImgDLG.cpp
@@ -275,6 +275,61 @@ void on_mouse(int event, int x, int y, int flags, void *p)
 		cvPutText(img1, label2, cvPoint(x, y + 20), &font2, CV_RGB(255, 255, 255));
 		cvShowImage("view", img1);
 	}
+	static Point pre_pt = (-1, -1);//初始坐标  
+	static Point cur_pt = (-1, -1);//实时坐标  
+	char temp[16];
+	Mat img, imgt;
+
+	if (event == CV_EVENT_LBUTTONDOWN)//左键按下，读取初始坐标，并在图像上该点处划圆  
+	{
+		g_dst.copyTo(img);//将原始图片复制到img中  
+		sprintf_s(temp, "(%d,%d)", x, y);
+		pre_pt = Point(x, y);
+		putText(img, temp, pre_pt, FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0, 255), 1, 8);//在窗口上显示坐标  
+		circle(img, pre_pt, 2, Scalar(255, 0, 0, 0), CV_FILLED, CV_AA, 0);//划圆  
+		imshow("view", img);
+	}
+	else if (event == CV_EVENT_MOUSEMOVE && !(flags & CV_EVENT_FLAG_LBUTTON))//左键没有按下的情况下鼠标移动的处理函数  
+	{
+
+		g_dst.copyTo(imgt);//将img复制到临时图像tmp上，用于显示实时坐标  
+		sprintf_s(temp, "(%d,%d)", x, y);
+		cur_pt = Point(x, y);
+		putText(imgt, temp, cur_pt, FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0, 255));//只是实时显示鼠标移动的坐标  
+		imshow("view",imgt);
+	}
+	else if (event == CV_EVENT_MOUSEMOVE && (flags & CV_EVENT_FLAG_LBUTTON))//左键按下时，鼠标移动，则在图像上划矩形  
+	{
+		g_dst.copyTo(imgt);
+		sprintf_s(temp, "(%d,%d)", x, y);
+		cur_pt = Point(x, y);
+		putText(imgt, temp, cur_pt, FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0, 255));
+		rectangle(imgt, pre_pt, cur_pt, Scalar(0, 255, 0, 0), 1, 8, 0);//在临时图像上实时显示鼠标拖动时形成的矩形  
+		imshow("view", imgt);
+	}
+	else if (event == CV_EVENT_LBUTTONUP)//左键松开，将在图像上划矩形  
+	{
+		g_dst.copyTo(img);
+		sprintf_s(temp, "(%d,%d)", x, y);
+		cur_pt = Point(x, y);
+		putText(img, temp, cur_pt, FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0, 255));
+	//	circle(img, pre_pt, 2, Scalar(255, 0, 0, 0), CV_FILLED, CV_AA, 0);
+		rectangle(img, pre_pt, cur_pt, Scalar(0, 255, 0, 0), 1, 8, 0);//根据初始点和结束点，将矩形画到img上  
+		imshow("view", img);
+		//img.copyTo(imgt);
+		//截取矩形包围的图像，并保存到dst中  
+	/*	int width = abs(pre_pt.x - cur_pt.x);
+		int height = abs(pre_pt.y - cur_pt.y);
+		if (width == 0 || height == 0)
+		{
+			printf("width == 0 || height == 0");
+			return;
+		}
+		dst = img(Rect(min(cur_pt.x, pre_pt.x), min(cur_pt.y, pre_pt.y), width, height));
+		namedWindow("dst");
+		imshow("dst", dst);
+		waitKey(0);*/
+	}
 }
 
 void CImgDLG::OnBnClickedShow()
